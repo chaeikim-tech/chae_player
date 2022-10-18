cf8d061fddd0cefaa9315bedc933e2838966d4d6
chaeikim <sb02348@hanmail.net>
2022-10-19 05:22:27 +0900

    Modified API bugs

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { isPlayingAtom } from '../atom';
-import { useRecoilState } from 'recoil';
-import styled from "styled-components"
-import { ImPlay2, ImPause } from "react-icons/im"
+import { useRecoilState, useRecoilValue } from 'recoil';
+import styled from "styled-components";
+import { ImPlay2, ImPause } from "react-icons/im";
+import axios from 'axios';
 
 const Wrapper = styled.div`
     left:0;
@@ -71,27 +72,53 @@ const MusicText = styled.div`
 `
 
 const Playlist = () => {
+    const [data, setData] = useState([]);
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingAtom);
+    const BASE_URL = 'https://shazam-core.p.rapidapi.com/v1'
 
     const togglePlay = () => {
         setIsPlaying(!isPlaying)
+    };
+
+    const options = {
+        method: 'GET',
+        url: `${BASE_URL}/charts/country`,
+        params: { country_code: 'KR' },
+        headers: {
+            'X-RapidAPI-Key': process.env.REACT_APP_SHAZAM_API_KEY,
+            'X-RapidAPI-Host': process.env.REACT_APP_SHAZAM_HOST
+        }
+    };
+
+    async function getData() {
+        await axios.request(options)
+            .then((res) => { setData(res.data); console.log(res.data); })
+            .catch((error) => console.error(error));
     }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
 
     return (
         <Wrapper>
-            <MusicComp>
-                <span style={{ fontSize: 22, fontWeight: 700, paddingRight: "35px" }}>1</span>
-                <ImgContainer onClick={togglePlay}>
-                    <MusicImg src={"https://image.yes24.com/goods/66904853/XL"} />
-                    {isPlaying ? <StyledImPlay size={40} /> : <StyledImPause size={40} />}
-                </ImgContainer>
-                <MusicText>
-                    <div style={{ fontSize: 20, fontWeight: 600 }}>Welcome To My Playground</div>
-                    <div style={{ fontSize: 15, fontWeight: 600 }}>NCT 127</div>
-                </MusicText>
-
-            </MusicComp>
+            {data?.map((item, idx) => (
+                <MusicComp key={item.key}>
+                    <span style={{ fontSize: 22, fontWeight: 700, paddingRight: "35px" }}>{idx + 1}</span>
+                    <ImgContainer onClick={togglePlay}>
+                        <MusicImg src={item.images.coverart} />
+                        {isPlaying ? <StyledImPlay size={40} /> : <StyledImPause size={40} />}
+                    </ImgContainer>
+                    <MusicText>
+                        <div style={{ fontSize: 20, fontWeight: 600 }}>{item.title}</div>
+                        <div style={{ fontSize: 15, fontWeight: 600 }}>{item.subtitle}</div>
+                    </MusicText>
+                </MusicComp>
+            ))}
         </Wrapper >
+
+
     )
 }
 
